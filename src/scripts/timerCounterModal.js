8fd96e4c43dbe8f2c765e1e1ea23e5b384f926e6
Kris Mendoza <thekittengallery@gmail.com>
2024-10-27 05:19:32 +0800

    Add reset total time functionality - Implements confirmation modal before resetting total time

--- src/scripts/timerCounterModal.js
+++ src/scripts/timerCounterModal.js
@@ -12,6 +12,12 @@ const deleteLastEntryBtn = document.getElementById('counter-modal-revert-btn');
 const closeEditCounterModalBtn = document.getElementById(
   'counter-modal-close-btn'
 );
+const resetTotalTimeBtn = document.getElementById('counter-modal-reset-btn');
+const resetConfirmationModal = document.getElementById('counter-reset-modal');
+const yesResetConfirmationBtn = document.getElementById(
+  'counter-reset-yes-btn'
+);
+const noResetConfirmationBtn = document.getElementById('counter-reset-no-btn');
 
 export const openEditCounterModal = function () {
   editCounterModal.showModal();
@@ -30,7 +36,24 @@ const deleteLastEntry = function () {
   renderTotalTimeInModal();
 };
 
+const resetTotalTimeCounter = function () {
+  counterValues.totalTimeInSeconds = 0;
+  counterValues.timeToAddInSeconds = 0;
+  counterValues.isNewTimeAdded = false;
+  renderTotalTime();
+  renderTotalTimeInModal();
+  resetConfirmationModal.close();
+  editCounterModal.close();
+};
+
 deleteLastEntryBtn.addEventListener('click', deleteLastEntry);
 closeEditCounterModalBtn.addEventListener('click', () =>
   editCounterModal.close()
+);
+resetTotalTimeBtn.addEventListener('click', () =>
+  resetConfirmationModal.showModal()
+);
+yesResetConfirmationBtn.addEventListener('click', resetTotalTimeCounter);
+noResetConfirmationBtn.addEventListener('click', () =>
+  resetConfirmationModal.close()
 );
